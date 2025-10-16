8eeed070c7fb930f04eee28af294ba7888873f2d
Marcos Felipe Sousa dos santos <okissidamaph3@gmail.com>
2025-10-16 18:15:35 -0300

    Atualização

--- src/main/java/com/marcosfelipe/usuario/business/UsuarioService.java
+++ src/main/java/com/marcosfelipe/usuario/business/UsuarioService.java
@@ -3,19 +3,9 @@ package com.marcosfelipe.usuario.business;
 import com.marcosfelipe.usuario.business.converter.UsuarioConverter;
 import com.marcosfelipe.usuario.business.dto.UsuarioDTO;
 import com.marcosfelipe.usuario.infrastructure.entity.Usuario;
-import com.marcosfelipe.usuario.infrastructure.repository.EnderecoRepository;
-import com.marcosfelipe.usuario.infrastructure.repository.TelefoneRepository;
 import com.marcosfelipe.usuario.infrastructure.repository.UsuarioRepository;
-import com.marcosfelipe.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authorization.AuthorizationDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,46 +15,26 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtUtil jwtUtil;
-    private final AuthenticationManager authenticationManager;
-    private final EnderecoRepository enderecoRepository;
-    private final TelefoneRepository telefoneRepository;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
         emailExiste(usuarioDTO.getEmail());
         usuarioDTO.setSenha(bCryptPasswordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
-        return usuarioConverter.paraUsuarioDTO(
-                usuarioRepository.save(usuario)
-        );
-    }
-
-    public String autenticarUsuario(UsuarioDTO usuarioDTO) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
-                            usuarioDTO.getSenha())
-            );
-            return "Bearer " + jwtUtil.generateToken(authentication.getName());
-
-        } catch (BadCredentialsException | UsernameNotFoundException | AuthorizationDeniedException e) {
-            throw new UnauthorizedException("Usuário ou senha inválidos: ", e.getCause());
-        }
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 
     public void emailExiste(String email) {
         try {
             boolean existe = verificaEmailExistente(email);
             if (existe) {
-                throw new ConflictException("Email já cadastrado " + email);
+                throw new ConflictException("Email já cadastrado: " + email);
             }
         } catch (ConflictException e) {
-            throw new ConflictException("Email já cadastrado ", e.getCause());
+            throw new ConflictException("Email já cadastrado", e.getCause());
         }
     }
 
     public boolean verificaEmailExistente(String email) {
-
         return usuarioRepository.existsByEmail(email);
     }
 }
